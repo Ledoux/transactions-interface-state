adbce5ae687c20c4f472bfd6fdf32a210de8c352
erwanledoux <lawanledoux@gmail.com>
2017-08-16 00:22:53 +0200

    fixed import

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-import * as flash from './reducers/flash'
-import * as information from './reducers/information'
-import * as modal from './reducers/modal'
-import * as navigation from './reducers/navigation'
-const pipeline = require('./reducers/pipeline').default
-import * as reselector from './reducers/reselector'
-import * as apis from './utils/apis'
-import * as automatic from './utils/automatic'
-import * as location from './utils/location'
-import * as props from './utils/props'
-import * as viewer from './utils/viewer'
-
-const transactionsInterfaceState = Object.assign({}, apis,
-  automatic,
-  flash,
-  information,
-  location,
-  modal,
-  navigation,
-  pipeline,
-  props,
-  reselector,
-  viewer
-)
-
-export default transactionsInterfaceState
+export * from './reducers/flash'
+export * from './reducers/information'
+export * from './reducers/modal'
+export * from './reducers/navigation'
+export * from './reducers/reselector'
+export * from './utils/apis'
+export * from './utils/automatic'
+export * from './utils/location'
+export * from './utils/props'
+export * from './utils/viewer'
--- src/reducers/pipeline.js
+++
@@ -1,7 +0,0 @@
-import { getReducerPackage } from 'transactions-redux-normalizer'
-
-// automatic packager
-const reducerPackage = getReducerPackage('pipeline')
-
-// export
-export default reducerPackage
